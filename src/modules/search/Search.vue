d75e54bd92ca1c60b7b32d583ac8cb6930d19d6d
pavel <sgsdfg@gmail.com>
2025-03-20 02:57:19 +0500

    F-00001 Contacts and remove excess styles

--- src/modules/search/Search.vue
+++ src/modules/search/Search.vue
@@ -1,33 +1,41 @@
 <template>
-  <div class="s-input-box">
-    <div class="s-search-svg">
-      <img class="s-svg-holder" src="/svg/search.svg" alt="post-stub">
-    </div>
-    <input
-        ref="inputRef"
-        v-model="searchConditionsStore.searchTerm"
-        type="text"
-        class="s-input"
-        autocomplete="off"
-        placeholder="Search..."
-    />
-    <div class="s-input-btn" @click.stop="toggleTools">
-      <img class="s-svg-holder" src="/svg/filters.svg" alt="post-stub">
-    </div>
-
-    <transition name="filters-slide">
-      <div v-if="showTools" class="tools-dropdown">
-        <div class="tools-section">
-          <h3 class="tools-section-title">Tagpicker</h3>
-          <TagsPicker></TagsPicker>
-        </div>
-        <div class="tools-section">
-          <h3 class="tools-section-title">Datepicker</h3>
-          <DatePicker></DatePicker>
-        </div>
-        <button class="tools-hide-btn" @click.stop="toggleTools">Hide</button>
+  <div class="s-and-contacts">
+    <div class="s-input-box">
+      <div class="s-search-svg">
+        <img class="s-svg-holder" src="/svg/search.svg" alt="post-stub">
       </div>
-    </transition>
+      <input
+          ref="inputRef"
+          v-model="searchConditionsStore.searchTerm"
+          type="text"
+          class="s-input"
+          autocomplete="off"
+          placeholder="Search..."
+      />
+      <div class="s-input-btn" @click.stop="toggleTools">
+        <img class="s-svg-holder" src="/svg/filters.svg" alt="post-stub">
+      </div>
+
+      <transition name="filters-slide">
+        <div v-if="showTools" class="tools-dropdown">
+          <div class="tools-section">
+            <h3 class="tools-section-title">Tagpicker</h3>
+            <TagsPicker></TagsPicker>
+          </div>
+          <div class="tools-section">
+            <h3 class="tools-section-title">Datepicker</h3>
+            <DatePicker></DatePicker>
+          </div>
+          <button class="tools-hide-btn" @click.stop="toggleTools">Hide</button>
+        </div>
+      </transition>
+    </div>
+    <a class="s-input-btn-contacts" href="https://github.com/PavelKotenev/blog_backend" target="_blank" rel="noopener noreferrer">
+      <img class="s-svg-holder" src="/svg/github.svg" alt="GitHub">
+    </a>
+    <a class="s-input-btn-contacts" href="https://t.me/SamDev030" target="_blank" rel="noopener noreferrer">
+      <img class="s-svg-holder" src="/svg/telegram.svg" alt="Telegram">
+    </a>
   </div>
 </template>
 
